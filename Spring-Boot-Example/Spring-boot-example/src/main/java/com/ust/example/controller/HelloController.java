9dae38fb9f9211c18dc48e04129b5f1e7d058d7e
mithralekshmi <87968294+mithralekshmi@users.noreply.github.com>
2021-08-10 18:00:46 -1200

    Add files via upload

---
+++ Spring-Boot-Example/Spring-boot-example/src/main/java/com/ust/example/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.ust.example.controller;
+
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	 @RequestMapping("/")
+	 public String Hello() {
+		 return"this is a java application";
+	 }
+	
+
+}
